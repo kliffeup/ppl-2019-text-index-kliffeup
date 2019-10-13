58298a87ce0b68e3fe5b87ac4dde02275eebe094
azatv <azatvaleev356778@gmail.com>
2019-10-14 02:21:21 +0300

    Text Index by kliffeup update after code review

--- add/text_index.py
+++ add/text_index.py
@@ -2,23 +2,10 @@ import re
 from typing import List
 from sort_functions import sorting_by_order, sorting_by_frequency
 
-
-def page_turner():
-    """ Процедура, "переворачивающая" страницу
-
-        Если номер текущей считанной строки - 46, увеличиваем текущее значение страницы,
-        обнуляя счетчик строк.
-
-    """
-    global page_number, line_number
-    if line_number == 46:
-        line_number = 0
-        page_number += 1
-
-
-dictionary = {}
+vocabulary = {}
 page_number = 1
 line_number = 0
+page_size = 46
 print('Здравствуйте, Вас приветствует Составитель индекса текста!')
 print('Желаете сохранять регистр символов при составлении индекса?')
 print('Да - введите "1", Нет - введите "0":')
@@ -31,7 +18,7 @@ if small_words:
 else:
     small_words = 4
 print('Желаете составить список наиболее часто встречающихся слов?')
-print('Да - введите число позиций в этом списке, Нет - введите "0":')
+print('Да - введите ЦЕЛОЕ число позиций в этом списке, Нет - введите "0":')
 top_words = int(input())
 count_of_words = 0
 print('Введите свой текст ниже, для окончания считывания переведите строку после последней строки текста'
@@ -43,37 +30,38 @@ while line != 'end of text input':
         line = line.lower()
     if line:
         line_number += 1
-        page_turner()
+        if line_number == page_size:
+            line_number = 1
+            page_number += 1
         words: List[str] = re.split("[, \"/\[\]()—!?.:;*]+", line)
         for i in words:
             if i and len(i) >= small_words:
-                if i not in dictionary:
+                if i not in vocabulary:
                     count_of_words += 1
-                    dictionary[i] = [1, [page_number]]
+                    vocabulary[i] = [1, [page_number]]
                 else:
-                    dictionary[i][0] += 1
-                    if page_number not in dictionary[i][1]:
-                        dictionary[i][1].append(page_number)
+                    vocabulary[i][0] += 1
+                    if page_number not in vocabulary[i][1]:
+                        vocabulary[i][1].append(page_number)
     line = input()
-print(dictionary)
 with open("Output.txt", 'w', encoding='utf-8') as output_data:
-    sorted_by_order = sorting_by_order(dictionary)
-    for i in sorted_by_order:
-        output_data.write('%s встречено на следующих страницах:' % i)
-        for j in range(len(dictionary[i][1])):
-            if j != len(dictionary[i][1]) - 1:
-                output_data.write(' %s,' % dictionary[i][1][j])
+    sorted_by_order = sorting_by_order(vocabulary)
+    output_data.write("Алфавитный индекс слов, встретившихся в введённом тексте:\n")
+    for k in sorted_by_order:
+        output_data.write(f"{k} встречено на следующих страницах:")
+        for j in range(len(vocabulary[k][1])):
+            if j != len(vocabulary[k][1]) - 1:
+                output_data.write(f" {vocabulary[k][1][j]},")
             else:
-                output_data.write(' %s;' % dictionary[i][1][j])
+                output_data.write(f" {vocabulary[k][1][j]};")
         output_data.write('\n')
     if top_words:
-        output_data.write('Список из %s наиболее встречающихся cлов в тексте:\n' % top_words)
-        sorted_by_frequence = sorting_by_frequency(dictionary)
-        print(sorted_by_frequence)
+        output_data.write(f'Список из {top_words} наиболее встречающихся cлов в тексте:\n')
+        sorted_by_frequence = sorting_by_frequency(vocabulary)
         if top_words <= count_of_words:
             n = top_words
         else:
             n = count_of_words
         for a in range(n):
             output_data.write(
-                '%s было встречено %s раз(а)\n' % (sorted_by_frequence[a][0], sorted_by_frequence[a][1][0]))
+                f'{sorted_by_frequence[a][0]} было встречено {sorted_by_frequence[a][1][0]} раз(а)\n')
